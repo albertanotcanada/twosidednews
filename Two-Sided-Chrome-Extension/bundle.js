80d6fa49c3c6fb8e6ff825a6e053bfc7171b0b5f
ejiao <edward_jiao@brown.edu>
2017-02-05 11:12:47 -0500

    added message about articles that couldn't be found

--- Two-Sided-Chrome-Extension/bundle.js
+++ Two-Sided-Chrome-Extension/bundle.js
@@ -7,6 +7,9 @@ var sources = {
     "www.breitbart.com": "conservative", 
     "www.drudgereport.com": "conservative", 
     "www.newyorker.com": "liberal", 
+    "www.buzzfeed.com": "liberal", 
+    "www.theatlantic.com": "liberal", 
+    "www.washingtonpost.com": "liberal", 
     "www.nytimes.com": "liberal", 
     "www.cnn.com": "liberal",
     "www.huffingtonpost.com": "liberal", 
@@ -24,37 +27,48 @@ chrome.tabs.query({'active': true}, function (tabs) {
     console.log("site: " + site); 
     /* google custom search */ 
     var query = title; 
-    //var key = "AIzaSyBz4G8Z4mocrtPLPX8kFyJ1NvTyV8J44Sc"; //key 
-    var key = "AIzaSyAgfhTgZhs1Vr9u8vP6MLnjIsYHvfcTc3s"; 
+    // var key = "AIzaSyBz4G8Z4mocrtPLPX8kFyJ1NvTyV8J44Sc"; //key 
+    // var key = "AIzaSyAgfhTgZhs1Vr9u8vP6MLnjIsYHvfcTc3s"; 
+    // var key = "AIzaSyDvQ7P_rS2Yh28FQCUrqNYQlZKKCwDclW0"; 
+    var key = "AIzaSyCrfL2c9Blh0HHGCvwmDa_-tE0S5RDpgmI";
     var searchID; 
+    var goAhead = true; 
     if (sources[site] == "liberal") { 
         searchID = "013199845727965234313:allhxzwrat4" ; // search conservative 
     } else if (sources[site] == "conservative") { 
         searchID = "003349935858378228433%3Awuei2zd_tuc"; // search liberal 
     } else { 
-
+        goAhead = false; 
+        $("#message").html("<b> Sorry, we couldn't find an article with similar content. </b>"); 
     }
 
-    // Google custom search api 
-    $.get("https://www.googleapis.com/customsearch/v1?q="+query+"&cx=" + searchID + "&key=" + key, function(data, status){
-        console.log("Data: " + data + "\nStatus: " + status); 
-        //$("#title").innerHTML = data.items[0].title; 
-        document.getElementById('title').innerHTML = data.items[0].title;
-        document.getElementById('title').href = data.items[0].link;
-        console.log(data.items[0].title);
-        console.log(data);
-        /*
-        document.getElementById('link').innerHTML = data.items[0].link;
-        document.getElementById('link').href = data.items[0].link;
-        */ 
-        document.getElementById('source').innerHTML = "from " + data.items[0].displayLink;
-        if (data.items[0].description != "undefined") { 
-            document.getElementById('description').innerHTML = data.items[0].snippet; 
-        } 
-        //$("#link").innerHTML = data.items[0].link; 
-        console.log("link: " + data.items[0].link);
+    if (goAhead) {
+        // Google custom search api 
+        $.get("https://www.googleapis.com/customsearch/v1?q="+query+"&cx=" + searchID + "&key=" + key, function(data, status){
+            console.log(data);
+            if (data == null) { 
 
-    });
+            } else { 
+                console.log("Data: " + data + "\nStatus: " + status); 
+                //$("#title").innerHTML = data.items[0].title; 
+                document.getElementById('title').innerHTML = data.items[0].title;
+                document.getElementById('title').href = data.items[0].link;
+                console.log(data.items[0].title);
+                console.log(data);
+                /*
+                document.getElementById('link').innerHTML = data.items[0].link;
+                document.getElementById('link').href = data.items[0].link;
+                */ 
+                document.getElementById('source').innerHTML = "from " + data.items[0].displayLink;
+                if (data.items[0].description != "undefined") { 
+                    document.getElementById('description').innerHTML = data.items[0].snippet; 
+                } 
+                //$("#link").innerHTML = data.items[0].link; 
+                console.log("link: " + data.items[0].link);
+            }
+
+        });
+    } 
     console.log("done!");
 
      
